68b34f310c1e9b7bd19bd2b26406d210c0528ffd
taleiabragagnolo <taleiabragagnolo@gmail.com>
2026-07-23 23:06:21 -0300

    Primeira tela do Organiza 3D Manager.

---
+++ app.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const botao = document.querySelector("button");
+
+    botao.addEventListener("click", () => {
+
+        alert("Bem-vinda ao Organiza 3D Manager!\n\nVamos começar cadastrando sua primeira impressora 3D.");
+
+    });
+
+});
